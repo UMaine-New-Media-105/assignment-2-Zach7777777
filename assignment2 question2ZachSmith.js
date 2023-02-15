083b5f85c93804b200ea4dc4d1ada518d1398a59
Zach7777777 <124187319+Zach7777777@users.noreply.github.com>
2023-02-15 15:16:07 -0500

    Drew a z using the beginShape function

---
+++ assignment2 question2ZachSmith.js
@@ -0,0 +1,22 @@
+//Assignment 2 question 2 Zach Smith
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100,0,300);
+  fill("lightslategrey")
+  beginShape();
+vertex(30, 20);
+vertex(350, 20);
+vertex(350, 75);
+vertex(95,320)
+vertex(350,320)
+vertex(350,370)
+vertex(30,370)
+vertex(30,320)
+vertex(280,70)
+vertex(30, 75);
+endShape(CLOSE);
+}
